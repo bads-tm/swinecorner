e14a5683d7f0a79a2294a614fd0c7aa0808ef777
dw5 <29355584+dw5@users.noreply.github.com>
2023-08-19 20:57:59 +0300

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -2,9 +2,10 @@
 <title>Heya - S.W.I.N.E. Mods and more</title>
 
 <div class="main">
-        <h2>Meet new site</h2>
-    <p>modern and so cool</p>
+        <h2>Welcome! </h2>
+    <p>Meet new site, modern and so cool!</p>
     <h2>Some cool and exlusive content lives here too!</h2>
+    <h3>GitHub build wasn't playing nice under /swine...</h3>
 </div>
 
 <rightdefault/>
