2fb4f2f4de9a1421f242d89c981df235005b48aa
dw5 <29355584+dw5@users.noreply.github.com>
2023-08-19 19:11:18 +0300

    fix

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,8 +3,6 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   ssr: false, //When false  target: "static", // and static, nuxt generates a SPA
 
-  app: {
-    baseURL: '/swine/', // baseURL: '/<repository>/'
-    buildAssetsDir: 'assets', // don't use "_" at the begining of the folder name to avoids nojkill conflict
-  }
+  app: {baseURL: "/swine/"},
+
 })
