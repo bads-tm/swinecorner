fc8bb8b6e6eabfd845085b23a5d0036fd9aea377
dw5 <29355584+dw5@users.noreply.github.com>
2023-08-19 21:21:19 +0300

    upd

--- pages/index.vue
+++ pages/index.vue
@@ -3,9 +3,12 @@
 
 <div class="main">
         <h2>Welcome! </h2>
+        <hr>
     <p>Meet new site, modern and so cool!</p>
     <h2>Some cool and exlusive content lives here too!</h2>
-    <h3>GitHub build wasn't playing nice under /swine...</h3>
+    <h3>GitHub pages build wasn't playing nice under /swine/ some things work fine, others, links broken...</h3>
+    <hr>
+    <p>Powered by fancy nuxt...</p>
 </div>
 
 <rightdefault/>
